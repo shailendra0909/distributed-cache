c0fe600e85eb2fbf5c4a629aa09624051cd07ee1
Shailendra <shailendra.cs09@gmail.com>
2026-05-31 18:39:21 +0530

    initial setup

---
+++ src/main/java/sp/test/App.java
@@ -0,0 +1,13 @@
+package sp.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
